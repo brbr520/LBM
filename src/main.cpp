ab6f8239b5adbb37ed906abe34f34dae5becf4d0
Eric Chen <eric@ericnchen.com>
2012-02-16 00:54:05 +0100

    basic node object initialization rewritten and finished

--- src/main.cpp
+++ src/main.cpp
@@ -4,97 +4,49 @@
  * This is a simple Lattice Boltzmann solver written for a class.
  *
  * Eric Chen (eric.chen@rwth-aachen.de)
- * Updated 02 February 2012
+ * Updated 16 February 2012
  *
  * Released under the MIT License, see included LICENSE file for more info.
  */
 
 #include <iostream>
 #include <vector>
-#include "tools.h"
+#include "input.h"
 #include "node.h"
+//#include "tools.h"
 
 int main(int argc, char *argv[]) {
 
-    check_input(argc);
-    std::vector<std::vector<Node> > lattice = create_lattice(argc, argv);
-    std::vector<std::vector<Node> > lattnew = create_lattice(argc, argv);
+    Input inputs;
+    Node testnode;
 
-    // print out initial state for homework
-    // these will be removed but are shown here to prove that i did
-    // the assignment correctly
-    std::cout << "printing u ..." << std::endl;
-    for (int j = 0; j < atoi(argv[2])+2; j++) {
-        for (int i = 0; i < atoi(argv[1])+2; i++) {
-            std::cout << lattice[j][i].u[0] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    std::cout << "printing v ..." << std::endl;
-    for (int j = 0; j < atoi(argv[2])+2; j++) {
-        for (int i = 0; i < atoi(argv[1])+2; i++) {
-            std::cout << lattice[j][i].u[1] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    std::cout << "printing rho ..." << std::endl;
-    for (int j = 0; j < atoi(argv[2])+2; j++) {
-        for (int i = 0; i < atoi(argv[1])+2; i++) {
-            std::cout << lattice[j][i].rho << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
 
-    // time loop
-    for (int timestep = 0; timestep < 100; timestep++) {
-        apply_boundary_conditions(lattice, argv);
-        apply_boundary_conditions(lattnew, argv);
-        for (int j = 1; j < atoi(argv[2])+1; j++) {
-            for (int i = 1; i < atoi(argv[1])+1; i++) {
-                stream(lattice, i, j);
-                lattnew[j][i].rho = lattice[j][i].calculate_density();
-                lattnew[j][i].u   = lattice[j][i].calculate_velocity();
-                lattnew[j][i].feq = lattice[j][i].calculate_feq();
-                lattnew[j][i].f   = lattice[j][i].feq;
-            }
-        }
-        lattice = lattnew;
-    }
-    
-    // print out final state for homework
-    // these will be removed but are shown here to prove that i did
-    // the assignment correctly
-    std::cout << "printing u ..." << std::endl;
-    for (int j = 0; j < atoi(argv[2])+2; j++) {
-        for (int i = 0; i < atoi(argv[1])+2; i++) {
-            std::cout << lattice[j][i].u[0] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    std::cout << "printing v ..." << std::endl;
-    for (int j = 0; j < atoi(argv[2])+2; j++) {
-        for (int i = 0; i < atoi(argv[1])+2; i++) {
-            std::cout << lattice[j][i].u[1] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-    std::cout << "printing rho ..." << std::endl;
-    for (int j = 0; j < atoi(argv[2])+2; j++) {
-        for (int i = 0; i < atoi(argv[1])+2; i++) {
-            std::cout << lattice[j][i].rho << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
+//    std::vector<std::vector<Node> > lattice = create_lattice(inputs);
 
-    std::cout << "Note! The velocities are different, because they" << std::endl;
-    std::cout << "were called before the boundary conditions were" << std::endl;
-    std::cout << "applied! i.e., these are the initialized values." << std::endl;
+
+
+//    std::vector<std::vector<Node> > lattnew = create_lattice(argc, argv);
+//    double gradient(calculate_gradient(argc, argv));
+//
+//    // time loop
+//    for (int timestep = 0; timestep < 100; timestep++) {
+//        for (int j = 1; j < atoi(argv[2])+1; j++) {
+//            for (int i = 1; i < atoi(argv[1])+1; i++) {
+//                stream(lattice, lattnew, i, j);
+//                lattnew[j][i].rho = lattice[j][i].calculate_density();
+//                lattnew[j][i].u   = lattice[j][i].calculate_velocity();
+//                lattnew[j][i].feq = lattice[j][i].calculate_feq();
+//                lattnew[j][i].f   = lattice[j][i].feq;
+//            }
+//        }
+//        if (check_convergence(lattice, lattnew, atoi(argv[1]), atoi(argv[2])) == true) {
+//            std::cout << "conv reached after " << timestep << " steps" << std::endl;
+//            break;
+//        } else {
+//            lattice = lattnew;
+//            apply_boundary_conditions(lattice, argv); // check if this belongs HERE
+//        }
+//    }
 
     return EXIT_SUCCESS;
 }
--- src/node.h
+++ src/node.h
@@ -4,7 +4,7 @@
  * Defines a class called node whith definitions for various properties.
  *
  * Eric Chen (eric.chen@rwth-aachen.de)
- * Updated 02 February 2012
+ * Updated 16 February 2012
  *
  * Released under the MIT License, see included LICENSE file for more info.
  */
@@ -12,25 +12,23 @@
 #ifndef NODE_H 
 #define NODE_H
 
-#include <vector>
+#include "input.h"
 
 class Node {
     public:
-    double rho;
-    std::vector<double> u, f, feq;
-    Node(double u0, double v0, double rho0) {
-        initialize(u0, v0, rho0);
-        feq  = calculate_feq();     // homework 5
-        f = feq;                    // homework 5
-        rho = calculate_density();  // homework 5
-        u = calculate_velocity();   // homework 5
+    // constructor
+    Node(Input inputs) {
+        initialize(inputs);
     }
-    double calculate_density(void);
-    std::vector<double> calculate_velocity(void);
-    std::vector<double> calculate_feq(void);
+
+//    double calculate_density(void);
+//    std::vector<double> calculate_velocity(void);
 
     private:
-    void initialize(double, double, double);
+    double u, v, rho;
+    std::vector<double> f, feq;
+    void initialize(Input);
+    void calculate_feq(void);
 };
 
 #endif
